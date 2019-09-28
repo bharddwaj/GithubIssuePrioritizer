2d2b05aa8c3da0fb53c9bba1a5a3c48256dbcfd0
Bharddwaj Vemulapalli <bharddwajvemulapalli@gmail.com>
2019-09-28 15:26:38 -0400

    starting the movement class

--- project.py
+++ project.py
@@ -1,7 +1,4 @@
-print("Hello World")
-import requests
-import json
-API_KEY = str("mFbi6aZ8dE6a5j3dKkZ3m8BXFsBXINFX")
-req = requests.get("http://api.giphy.com/v1/gifs/search?",{"api_key":API_KEY,"q":"cheeseburgers"})
 
+class Movement():
 
+    def __init__
