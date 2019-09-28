73597b50654efc3f2119776094ded5e2b5b09013
Bharddwaj Vemulapalli <bharddwajvemulapalli@gmail.com>
2019-09-28 18:55:42 -0400

    Added model code

--- model.py
+++ model.py
@@ -1,2 +1,12 @@
+import tensorflow as tf
+
 def run(data):
-    pass
+	features = [number_of_responses,length_of_issue]
+	label = 0
+#the below two data fields have to be imported from the GitHub Issues dataset
+number_of_responses = 3
+length_of_issue = 2
+#label can be 0,1,2 
+# 0 = High Priority
+# 1 = Medium Priority
+# 2 = Low Priority
