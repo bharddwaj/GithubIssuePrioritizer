6bceb0a4096aa858b0f7a00977a3057d583806a8
Bharddwaj Vemulapalli <bharddwajvemulapalli@gmail.com>
2019-09-28 19:35:33 -0400

    some model stuff

--- model.py
+++ model.py
@@ -1,12 +1,14 @@
 import tensorflow as tf
-
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 def run(data):
-	features = [number_of_responses,length_of_issue]
+	#features = [data['number_of_responses'],data['length_of_issue']]
+	#the below two data fields have to be imported from the GitHub Issues dataset
+
+	#label can be 0,1,2 
+	# 0 = High Priority
+	# 1 = Medium Priority
+	# 2 = Low Priority
+	number_of_responses = 3
+	length_of_issue = 2
 	label = 0
-#the below two data fields have to be imported from the GitHub Issues dataset
-number_of_responses = 3
-length_of_issue = 2
-#label can be 0,1,2 
-# 0 = High Priority
-# 1 = Medium Priority
-# 2 = Low Priority
+
