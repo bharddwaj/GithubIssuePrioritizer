b5e7413e04a73dfb694c0a04c946679169e7e57d
Bharddwaj Vemulapalli <bharddwajvemulapalli@gmail.com>
2019-09-29 11:17:57 -0400

    changed the model.py file

--- model.py
+++ model.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.linear_model import SGDClassifier,LogisticRegression
+from sklearn.svm import LinearSVC 
 from sklearn.utils import shuffle
 from sklearn.metrics import accuracy_score
 from preprocessing import preprocess
@@ -8,9 +9,13 @@ def run(path):
 	
 	X_train, X_test, X_val,y_val, y_train, y_test = preprocess(path)
 	clf = LogisticRegression(random_state=0, solver='lbfgs',multi_class='multinomial')
+	#clf = LinearSVC()
 	clf.fit(X_train,y_train)
 	y_pred = clf.predict(X_val)
-	return accuracy_score(y_val, y_pred)
+	val_accuracy = accuracy_score(y_val, y_pred)
+	y_pred = clf.predict(X_test)
+	test_accuracy = accuracy_score(y_test, y_pred)
+	return [val_accuracy,test_accuracy]
 
 
 def run2(path):
@@ -26,6 +31,8 @@ def run2(path):
 	model.fit(X_train,y_train, validation_data=(X_val, y_val))
 	accuracy = model.evaluate(X_test,y_test)
 	return accuracy
+
+
 
 if __name__ == "__main__":
 	print(run("normalized-github-issues.csv"))
