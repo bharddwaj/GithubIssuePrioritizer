8d6cc1682cad352b1ff1dbff42282d36d55af32a
Bharddwaj Vemulapalli <bharddwajvemulapalli@gmail.com>
2019-09-29 17:21:27 -0400

    deleted some uneccesary files

--- model.py
+++ model.py
@@ -31,19 +31,19 @@ def run(data):
 	return loaded_model.predict(data)
 	
 
-def run2(path):
-	model = tf.keras.models.Sequential([
-		tf.keras.layers.Flatten(input_shape=(28, 28)), #need to change input size
-		tf.keras.layers.Dense(128, activation='relu'),
-		tf.keras.layers.Dropout(0.2),
-		tf.keras.layers.Dense(10, activation='softmax')
-	])
+# def run2(path):
+# 	model = tf.keras.models.Sequential([
+# 		tf.keras.layers.Flatten(input_shape=(28, 28)), #need to change input size
+# 		tf.keras.layers.Dense(128, activation='relu'),
+# 		tf.keras.layers.Dropout(0.2),
+# 		tf.keras.layers.Dense(10, activation='softmax')
+# 	])
 
-	model.compile(optimizer='adam',loss='sparse_categorical_crossentropy',metrics=['accuracy'])
-	X_train, X_test, X_val,y_val, y_train, y_test = preprocess(path)
-	model.fit(X_train,y_train, validation_data=(X_val, y_val))
-	accuracy = model.evaluate(X_test,y_test)
-	return accuracy
+# 	model.compile(optimizer='adam',loss='sparse_categorical_crossentropy',metrics=['accuracy'])
+# 	X_train, X_test, X_val,y_val, y_train, y_test = preprocess(path)
+# 	model.fit(X_train,y_train, validation_data=(X_val, y_val))
+# 	accuracy = model.evaluate(X_test,y_test)
+# 	return accuracy
 
 
 
--- preprocessing.py
+++ preprocessing.py
@@ -19,8 +19,9 @@ def preprocess(path):
     # for i in range(len(X[0])):
     #     new_x.append((X[0][i],X[1][i]))
     new_x = DataFrame.from_records(X)
-    X_train, X_test, y_train, y_test = train_test_split(X[1], y, test_size=0.33, random_state=42)
-    X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.2, random_state=1)
+    #print(f"{X[1][0]}")
+    X_train, X_test, y_train, y_test = train_test_split(X[1], y, test_size=0.4, random_state=42)
+    X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.3, random_state=1)
 
     return X_train, X_test, X_val,y_val, y_train, y_test
 # TODO Fill this in with actual serialization and deserialization process
