c0db08f804401e51137f3824c6a3abef8677fdc0
Sattwik Tripathy <sattwik@10-16-242-79.dynapool.wireless.nyu.edu>
2019-09-28 21:15:13 -0400

    First Commitgit add .

---
+++ tfidf.py
@@ -0,0 +1,24 @@
+categories = ['alt.atheism', 'soc.religion.christian','comp.graphics', 'sci.med']
+
+from sklearn.datasets import fetch_20newsgroups
+twenty_train = fetch_20newsgroups(subset='train',categories=categories, shuffle=True, random_state=42)
+
+from sklearn.feature_extraction.text import CountVectorizer
+count_vect = CountVectorizer()
+X_train_counts = count_vect.fit_transform(twenty_train.data)
+#X_train_counts.shape(2257, 35788)
+
+from sklearn.feature_extraction.text import TfidfTransformer
+tf_transformer = TfidfTransformer(use_idf=False).fit(X_train_counts)
+X_train_tf = tf_transformer.transform(X_train_counts)
+#X_train_tf.shape(2257, 35788)
+
+tfidf_transformer = TfidfTransformer()
+X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
+#X_train_tfidf.shape(2257, 35788)
+
+from sklearn.pipeline import Pipeline
+text_clf = Pipeline([
+('vect', CountVectorizer()),
+('tfidf', TfidfTransformer()),
+])
