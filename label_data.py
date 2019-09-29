de56b367ceb8719c244fed20ba13eb20ecacf5e2
Albert Liu <albertymliu@gmail.com>
2019-09-29 11:34:06 -0400

    added labels as dummy variables

--- label_data.py
+++ label_data.py
@@ -17,27 +17,30 @@ def normalize_priority(label):
 
 
 def normalize_data(data):
+    data = data.copy()
     data['priority'] = data['priority'].map(normalize_priority)
     return data[data['priority'].notnull()]
 
 def pivot_labels(data):
+    data = data.copy()
     data['other_labels'] = data['other_labels'].str.split(',')
     data = data.explode('other_labels')
     data['other_labels'] = data['other_labels'].str.strip()
     data.drop(data['other_labels'] == '', axis=0)
-
     data['dummy'] = 1
-    data = data.pivot_table(values='dummy',
-                            columns='other_labels',
-                            index=['issue_title', 'body', 'priority'])
     print(data.columns)
+    print(data.shape)
+    data = data.pivot_table(values='dummy',
+                            columns=['other_labels'],
+                            index=['issue_title', 'body', 'priority'])
+    data.reset_index(level=data.index.names, inplace=True)
     return data
 
 
 def main():
     data = pd.read_csv("github-issues.csv")
     normalized_data = normalize_data(data)
-    normalized_label_data = pivot_labels(data)
+    normalized_label_data = pivot_labels(normalized_data)
     normalized_label_data.to_csv("normalized-github-issues.csv", index=False)
 
 
