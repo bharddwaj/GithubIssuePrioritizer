cd946b158530f7f176026e3aa24410979c41b2be
VeenaArv <vla240@nyu.edu>
2019-09-28 20:52:02 -0400

    Adds n_days_open column

--- import_data.sql
+++ import_data.sql
@@ -1,29 +1,22 @@
--- script based on How To Create Data Products That Are Magical Using Sequence-to-Sequence Models by Hamel Husain
--- gets issues with labels from the Github Archive
-
 SELECT
-  url AS issue_url
-  -- replace more than one white-space character in a row with a single space
-  ,
-  REGEXP_REPLACE(title, r"\s{2,}", ' ') AS issue_title,
-  REGEXP_REPLACE(body, r"\s{2,}", ' ') AS body,
-  names
+  REGEXP_REPLACE(title, r"\s{2,}", ',') AS issue_title,
+  REGEXP_REPLACE(body, r"\s{2,}", ',') AS body,
+  ARRAY_TO_STRING(names, ",") labels,
+  DATE_DIFF(time_closed, time_created, DAY) n_days_open
 FROM (
   SELECT
-    JSON_EXTRACT(payload,
-      '$.issue.html_url') AS url
-    --         extract the title and body removing parentheses, brackets, and quotes
-    ,
     LOWER(TRIM(REGEXP_REPLACE(JSON_EXTRACT(payload,
             '$.issue.title'), r"\\n|\(|\)|\[|\]|#|\*|`|\"", ' '))) AS title,
     LOWER(TRIM(REGEXP_REPLACE(JSON_EXTRACT(payload,
             '$.issue.body'), r"\\n|\(|\)|\[|\]|#|\*|`|\"", ' '))) AS body,
-    LOWER(TRIM(REGEXP_REPLACE(JSON_EXTRACT(payload,
-            '$.issue.labels.name'), r"\\n|\(|\)|\[|\]|#|\*|`|\"", ' '))) AS labels,
+            CAST( REGEXP_REPLACE(SPLIT(LOWER(JSON_EXTRACT(payload,
+            '$.issue.created_at')), 't')[OFFSET(0)], r"[^-0-9]", "")  as DATE) AS time_created,
+             CAST(REGEXP_REPLACE(SPLIT(LOWER(JSON_EXTRACT(payload,
+            '$.issue.closed_at')), "t")[OFFSET(0)], r"[^-0-9]", "") as DATE) AS time_closed,
     ARRAY(
     SELECT
       JSON_EXTRACT_SCALAR(split_items,
-        '$.name') AS name
+        '$.name') AS names
     FROM (
       SELECT
         CONCAT('{', REGEXP_REPLACE(split_items, r'^\[{|}\]$', ''), '}') AS split_items
@@ -43,7 +36,3 @@ WHERE
   ARRAY_LENGTH(SPLIT(body, ' ')) >= 6
   AND ARRAY_LENGTH(SPLIT(title, ' ')) >= 3
   AND ARRAY_LENGTH(names) >= 1
-  -- filter out issues that have really long titles or bodies
-  --    (these are outliers, and will slow tokenization down).
-  AND LENGTH(title) <= 400
-  AND LENGTH(body) <= 2000
