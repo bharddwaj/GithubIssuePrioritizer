0e638dd7a4b7559c7e59ad7c2cdaf57d02a8011a
Bharddwaj Vemulapalli <bharddwajvemulapalli@gmail.com>
2019-09-29 10:09:20 -0400

    updated preprocessing

--- preprocessing.py
+++ preprocessing.py
@@ -35,18 +35,16 @@ def filter_priority_labels(keywords):
 #print(len(indices))
 #print(len(data))
 
-def preprocess(data):
+def preprocess(path):
     ''':data a string of the file name (in the same path)
     should return features and labels that we want for training and test'''
-    data = pd.read_csv(data)
-    data = data.sample(frac=1).reset_index(drop=True)
-    new_data = data["time_difference","number_of_comments","labels"]
-    X = [new_data["time_difference"],new_data["number_of_comments"]]
-    y = new_data["labels"]
+    data = pd.read_csv(path)
+    X = data["time_difference","number_of_comments"]
+    y = data["labels"]
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
-    
+    X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.2, random_state=1)
 
-    return [X_train, X_test, y_train, y_test]
+    return [X_train, X_test, X_val,y_val, y_train, y_test]
 # TODO Fill this in with actual serialization and deserialization process
 model = None
 dump(model, 'models/model')
