01541cc17abb416b5c68ca5a52c3d9f31018928a
Bharddwaj Vemulapalli <bharddwajvemulapalli@gmail.com>
2019-09-29 10:29:13 -0400

    minor change to run function

--- model.py
+++ model.py
@@ -5,20 +5,11 @@ from sklearn.utils import shuffle
 from sklearn.metrics import accuracy_score
 from preprocessing import preprocess
 def run(path):
-	#features = [data['number_of_responses'],data['length_of_issue']]
-	#the below two data fields have to be imported from the GitHub Issues dataset
-
-	#label can be 0,1,2 
-	# 0 = High Priority
-	# 1 = Medium Priority
-	# 2 = Low Priority
-	number_of_responses = 3
-	length_of_issue = 2
-	label = 0
+	
 	X_train, X_test, X_val,y_val, y_train, y_test = preprocess(path)
 	clf = LogisticRegression(random_state=0, solver='lbfgs',multi_class='multinomial')
 	clf.fit(X_train,y_train)
 	y_pred = clf.predict(X_val)
-	accuracy_score(y_val, y_pred)
+	return accuracy_score(y_val, y_pred)
 
 
