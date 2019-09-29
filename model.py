fafdb2fae779bdf93f54e054d965ffb709097842
Bharddwaj Vemulapalli <bharddwajvemulapalli@gmail.com>
2019-09-29 10:19:22 -0400

    make the necessary changes to preprocess

--- model.py
+++ model.py
@@ -2,7 +2,9 @@ import tensorflow as tf
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.linear_model import SGDClassifier,LogisticRegression
 from sklearn.utils import shuffle
-def run(data):
+from sklearn.metrics import accuracy_score
+from preprocessing import preprocess
+def run(path):
 	#features = [data['number_of_responses'],data['length_of_issue']]
 	#the below two data fields have to be imported from the GitHub Issues dataset
 
@@ -13,7 +15,10 @@ def run(data):
 	number_of_responses = 3
 	length_of_issue = 2
 	label = 0
+	X_train, X_test, X_val,y_val, y_train, y_test = preprocess(path)
 	clf = LogisticRegression(random_state=0, solver='lbfgs',multi_class='multinomial')
-	clf.fit(train_features,train_labels)
+	clf.fit(X_train,y_train)
+	y_pred = clf.predict(X_val)
+	accuracy_score(y_val, y_pred)
 
 
--- preprocessing.py
+++ preprocessing.py
@@ -40,11 +40,11 @@ def preprocess(path):
     should return features and labels that we want for training and test'''
     data = pd.read_csv(path)
     X = data["time_difference","number_of_comments"]
-    y = data["labels"]
+    y = data["priority"]
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
     X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.2, random_state=1)
 
-    return [X_train, X_test, X_val,y_val, y_train, y_test]
+    return X_train, X_test, X_val,y_val, y_train, y_test
 # TODO Fill this in with actual serialization and deserialization process
 model = None
 dump(model, 'models/model')
