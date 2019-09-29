4855a673d717bf9fa24f17b693085a683bd75fd1
Sattwik Tripathy <sattwik@10-16-242-79.dynapool.wireless.nyu.edu>
2019-09-29 10:20:16 -0400

    classifier update

--- tfidf.py
+++ tfidf.py
@@ -1,11 +1,11 @@
 categories = ['high', 'medium','low', 'priority','bug']
 
 from sklearn.datasets import fetch_20newsgroups
-twenty_train = fetch_20newsgroups(subset='train',categories=categories, shuffle=True, random_state=42)
+train_data = fetch_20newsgroups(subset='train',categories=categories, shuffle=True, random_state=42)
 
 from sklearn.feature_extraction.text import CountVectorizer
 count_vect = CountVectorizer()
-X_train_counts = count_vect.fit_transform(twenty_train.data)
+X_train_counts = count_vect.fit_transform(train_data.data)
 #X_train_counts.shape(2257, 35788)
 
 from sklearn.feature_extraction.text import TfidfTransformer
@@ -17,8 +17,28 @@ tfidf_transformer = TfidfTransformer()
 X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
 #X_train_tfidf.shape(2257, 35788)
 
+from sklearn.naive_bayes import MultinomialNB
+clf = MultinomialNB().fit(X_train_tfidf, train_data.target)
+
+docs_new = ['God is love', 'OpenGL on the GPU is fast']
+X_new_counts = count_vect.transform(docs_new)
+X_new_tfidf = tfidf_transformer.transform(X_new_counts)
+
+predicted = clf.predict(X_new_tfidf)
+
+for doc, category in zip(docs_new, predicted):
+	print('%r => %s' % (doc, train_data.target_names[category]))
+
 from sklearn.pipeline import Pipeline
 text_clf = Pipeline([
 ('vect', CountVectorizer()),
 ('tfidf', TfidfTransformer()),
+('clf', MultinomialNB()),
 ])
+
+import numpy as np
+twenty_test = fetch_20newsgroups(subset='test',
+     categories=categories, shuffle=True, random_state=42)
+docs_test = twenty_test.data
+predicted = text_clf.predict(docs_test)
+np.mean(predicted == twenty_test.target)    
